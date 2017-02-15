f95b0b6e8cf3dc546af5decf0cbc34f1660ac6e7
Zaeem-Shafiq <zaeem1996@hotmail.com>
2017-02-15 12:04:14 +0100

    Added more tests

--- CA1/src/test/java/TestServer.java
+++ CA1/src/test/java/TestServer.java
@@ -1,88 +1,67 @@
 
 import com.mycompany.ca1.ChatServer;
-import java.io.BufferedReader;
+import com.mycompany.ca1.MyClient;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import org.junit.After;
-import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
  * @author zaeemshafiq
  */
 public class TestServer {
-    
+
     public TestServer() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
 //        new Thread(() -> {
 //            ChatServer cs = new ChatServer();
 //        }).start();
     }
-    
+
     @Before
-    public void setUp() throws IOException {
-//        new Thread(() -> {
-//            ChatServer cs = new ChatServer();
-//        }).start();
+    public void setUp()  {
+        new Thread(() -> {
+            ChatServer cs = new ChatServer();
+            cs.setClientConnection();
+        }).start();
     }
 
-     @Test
-     public void testLogin() throws IOException {
-        Socket socket = new Socket();
+    @Test
+    public void testLogin() throws  InterruptedException, IOException {
+        MyClient mc = new MyClient("localhost", 8081);
+        mc.open();
+        mc.sendMessage("LOGIN#Kim7");
+        String line = mc.readMessage();
+        
+        assertEquals("OK#Kim7", line);
+    }
 
-        socket.connect(new InetSocketAddress("vetterlain.dk", 8081));
-        OutputStream os = socket.getOutputStream();
-        PrintWriter out = new PrintWriter(os);
-        
-        out.println("LOGIN#Tom");
-        out.flush();
-        
-        
-        
-        InputStream is = socket.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        
-        String line = reader.readLine();
-        
-         System.out.println(line);
-        
-         assertEquals("OK#Tom", line);
-     }
-     
-     @Test
-     public void testMSGToOne() throws IOException {
-        Socket socket = new Socket();
-
-        socket.connect(new InetSocketAddress("vetterlain.dk", 8081));
-        OutputStream os = socket.getOutputStream();
-        PrintWriter out = new PrintWriter(os);
-        
-        out.println("LOGIN#Tom");
-        out.println("LOGIN#Tim");
-        
-        out.println("MSG#Tom#Hey!");
-        out.flush();
-        
-        InputStream is = socket.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        
-        String line = reader.readLine();
-        
-         System.out.println(line);
-        
-         assertEquals("OK#Tom", line);
-     }
+//     @Test
+//     public void testMSGToOne() throws IOException {
+//        Socket socket = new Socket();
+//
+//        socket.connect(new InetSocketAddress("vetterlain.dk", 8081));
+//        OutputStream os = socket.getOutputStream();
+//        PrintWriter out = new PrintWriter(os);
+//        
+//        out.println("LOGIN#Tom");
+//        out.println("LOGIN#Tim");
+//        
+//        out.println("MSG#Tom#Hey!");
+//        out.flush();
+//        
+//        InputStream is = socket.getInputStream();
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//        
+//        String line = reader.readLine();
+//        
+//         System.out.println(line);
+//        
+//         assertEquals("OK#Tom", line);
+//     }
 }
-    
