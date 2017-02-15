c3466860dda86e9de70b211e3e30d63a4a002dac
Joacim <Joacim@vetterlain.dk>
2017-02-15 12:22:12 +0100

    changed server

--- CA1/src/main/java/com/mycompany/ca1/StartServer.java
+++ CA1/src/main/java/com/mycompany/ca1/StartServer.java
@@ -2,6 +2,9 @@
 package com.mycompany.ca1;
 
 public class StartServer {
+    public static void main(String[] args) {
     ChatServer c = new ChatServer();
+        c.setClientConnection();
+    }
     
 }
