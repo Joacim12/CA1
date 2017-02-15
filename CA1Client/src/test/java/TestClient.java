4aa9a64c924913948c7d50c92bb24e940aca5ef1
Zaeem-Shafiq <zaeem1996@hotmail.com>
2017-02-15 10:29:44 +0100

    Added some tests

---
+++ CA1Client/src/test/java/TestClient.java
@@ -0,0 +1,28 @@
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author zaeemshafiq
+ */
+public class TestClient {
+    
+    public TestClient() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+
+    @Test
+    public void hello() {
+    }
+}
